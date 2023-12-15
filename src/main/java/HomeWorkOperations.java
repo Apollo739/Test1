34656440b4c8aefee4bead2b73c6322223b0708e
apollo <apoll0time739@gmail.com>
2023-12-16 00:29:51 +0900

    save home work

---
+++ src/main/java/HomeWorkOperations.java
@@ -0,0 +1,36 @@
+ class HomeWorkOperations {
+    public static void main (String[] args) {
+        for (int i = 1; i <= 5; i++){
+            System.out.print (i + "  ");
+        }
+    }
+}
+
+ class SumNumbers {
+    public static void main(String[] args) {
+
+//        int []ar = {1, 2, 3, 4, 5};
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
+class Operations{
+    public static void main(String[] args){
+        int a = 6;
+        int b = 1;
+        int resalt = 0;
+        if (b == 0) {
+
+            System.out.println("b is equals zero, please enter another number");
+        } else {
+             resalt = a / b;
+        }
+        System.out.println(resalt);
+    }
+
+}
